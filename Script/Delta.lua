debf0daa8879fc86577ff64fe91e8a22591b1511
okayysy <yuannprince@gmail.com>
2025-08-13 17:33:50 +0800

    Update Delta.lua

--- Script/Delta.lua
+++ Script/Delta.lua
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://pastefy.app/E9oeRkqE/raw"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/okayysy/Script-Dupe/refs/heads/main/Script/Lua.", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/okayysy/Dupe-Script/refs/heads/main/Script/GrowAGarden.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/okayysy/DUPE/refs/heads/main/Script"))()
